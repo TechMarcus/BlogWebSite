57f7b3f983eece5d95fe4a4967fe68a967ec1a0a
TechMarcus <ranger.ahilles@gmail.com>
2026-02-21 00:39:24 +0200

    test fix

--- app/backend/routers/users.py
+++ app/backend/routers/users.py
@@ -6,4 +6,4 @@ router = APIRouter(
 
 @router.get("/")
 async def read_users():
-    return [{"username": "Rickff"}, {"username": "Morty"}]
+    return [{"username": "Ricky"}, {"username": "Morty"}]
