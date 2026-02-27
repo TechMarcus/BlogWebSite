b035320c16e9781859ab909ab2d24b6e676373ef
TechMarcus <ranger.ahilles@gmail.com>
2026-02-27 19:49:44 +0200

    monitoring changes

--- infra/monitoring_configs/prometheus/entrypoint.sh
+++ infra/monitoring_configs/prometheus/entrypoint.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
-apk add --no-cache gettext  # if it's alpine-based
+apk add --no-cache gettext 
 envsubst < /etc/prometheus/prometheus.template.yml > /etc/prometheus/prometheus.yml
 exec prometheus "$@"
