8b1e3c2588077fe4646969860ed1d6de7c0a637b
TechMarcus <ranger.ahilles@gmail.com>
2026-02-27 20:25:55 +0200

    monitoring changes

--- infra/monitoring_configs/prometheus/entrypoint.sh
+++ infra/monitoring_configs/prometheus/entrypoint.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
-apk add --no-cache gettext --quiet
-envsubst < /etc/prometheus/prometheus.template.yml > /etc/prometheus/prometheus.yml
+sed -e "s|\${PROMETHEUS_USERNAME}|$PROMETHEUS_USERNAME|g" \
+    -e "s|\${PROMETHEUS_API_TOKEN}|$PROMETHEUS_API_TOKEN|g" \
+    /etc/prometheus/prometheus.template.yml > /etc/prometheus/prometheus.yml
+
 exec prometheus "$@"
